4e2ce96ea9dc49d60f220420ad939ccf98695ad6
Carlos Rodrigo Atl <carlosrodrigoatl@gmail.com>
2024-10-08 18:40:29 -0600

    Se agrego el dbdset y el controlador para la tabla games

---
+++ VideoGamesLibreryBack/VideoGamesLibreryBack/DbSet/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGamesLibreryBack.ModelDB;
+
+namespace VideoGamesLibreryBack.DbSet
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Games>()
+                .HasNoKey(); // Configura la entidad como sin clave
+        }
+
+        public DbSet<Games> Games { get; set; }
+    }
+}
