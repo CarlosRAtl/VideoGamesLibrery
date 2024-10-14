d8e568e39c90d6fd2a56eeb6ff3040391b63b322
Carlos Rodrigo Atl <carlosrodrigoatl@gmail.com>
2024-10-13 22:04:40 -0600

    Creación de los controladores para VideoGamesConsoles

---
+++ VideoGamesLibreryBack/VideoGamesLibreryBack/ModelDB/GameConsole.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VideoGamesLibreryBack.ModelDB
+{
+    [Table("VideoGameConsoles")]
+    public class GameConsole
+    {
+        [Key]
+        public string? ConsoleId { get; set; }
+        [StringLength(255)]
+        public string? NameConsole {  get; set; }
+        [StringLength(20)]
+        public string? Company {  get; set; }
+        public int Quantity { get; set; }
+        public bool IsEspecialEdition { get; set; }
+        public bool IsPortable { get; set; }
+        public bool IsNew { get; set; }
+        public bool IsOpen { get; set; }
+        public bool IsComplete { get; set; }
+        public bool IsProtected { get; set; }
+        public string? Description { get; set; }
+    }
+}
